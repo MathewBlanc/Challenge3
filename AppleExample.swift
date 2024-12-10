c7b25c7961efe0f3d8830e3e1b2ae7bac46b1cbb
Matt Blanc <mathewpblanc@gmail.com>
2024-12-10 15:28:25 +0100

    Added slider gesture and timer

---
+++ AppleExample.swift
@@ -0,0 +1,18 @@
+//
+//  AppleExample.swift
+//  Challenge3
+//
+//  Created by Mathew Blanc on 10/12/24.
+//
+
+import SwiftUI
+
+struct AppleExample: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppleExample()
+}
